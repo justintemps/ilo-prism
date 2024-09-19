63ec388ec7e2b5884e88696cb7356f37d1ed3ab7
justintemps <jesmith627@gmail.com>
2024-09-19 12:12:56 +0200

    Success! dynamically render gradio dropdowns

--- main.py
+++ main.py
@@ -7,20 +7,38 @@ ilostat_areas = ilostat.get_areas()
 
 
 with gr.Blocks() as demo:
-    dataflows_dropdown = None
-    areas_dropdown = gr.Dropdown(choices=ilostat_areas, label="Select an area")
 
-    @gr.render(inputs=areas_dropdown)
-    def get_dataflows(area):
-        global dataflows_dropdown
+    gr.Markdown("# Summarize a table from ILOSTAT")
+
+    # Which area to query data about
+    query_area = gr.State(None)
+
+    # Which dataflows to query
+    query_dataflow = gr.State(None)
+
+    # Areas dropdown menu
+    areas_dropdown = gr.Dropdown(
+        choices=ilostat_areas, label="First select a geographic region")
+
+    # Update the query_area state on change
+    areas_dropdown.input(
+        lambda area: area, areas_dropdown, query_area)
+
+    @gr.render(inputs=[query_area, query_dataflow])
+    def render_components(area, df):
+        # Once we select an area, now we can render the dataflows
         if area:
             dataflows = ilostat.get_dataflows(area)
             dataflows_dropdown = gr.Dropdown(
-                choices=dataflows, label="Select a dataflow")
+                choices=dataflows, label="Then select an indicator from ILOSTAT", value=df)
+            dataflows_dropdown.change(
+                lambda df: df, dataflows_dropdown, query_dataflow)
 
-    @gr.render(inputs=dataflows_dropdown)
-    def get_dataflow_description(something):
-        print("bullshit")
+        # Once we select the dataflows, now we can show the description
+        if df and len(df) != 0:
+            description = ilostat.get_dataflow_description(df)
+            gr.Markdown("### About this data")
+            gr.HTML(description)
 
 
 if __name__ == "__main__":
